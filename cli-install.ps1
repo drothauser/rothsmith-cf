14c8bd5285acf2e4ab88a4da1f588fc467af3bc3
Doug Rothauser <drothauser@yahoo.com>
2018-11-19 16:00:13 -0500

    development

--- cli-install.ps1
+++ cli-install.ps1
@@ -1,21 +1,23 @@
-
 function Install-CLI
 {
 
     $NCCT_Software_Bucket = "ncct-software";
-    $AWSCLI_Key = "AWSCLI64PY3.msi"
+    $AWSCLI_Installer_File = "AWSCLI64PY3.msi"
     $CIS_Directory = "c:\cis";
     $SetRegion ="us-gov-west-1";
+    $SetLocation_AwsCLI = "$CIS_Directory\EC2-Software\AwsCLI";
+
+    Set-Location $SetLocation_AwsCLI
 
     read-s3object `
         -BucketName $NCCT_Software_Bucket `
-        -Key $AWSCLI_Key `
-        -File $CIS_Directory\$AWSCLI_Key `
+        -Key $AWSCLI_Installer_File `
+        -File $SetLocation_AwsCLI\$AWSCLI_Installer_File `
         -Region $SetRegion
 
-    start-process $CIS_Directory\$AWSCLI_Key /qn -Wait
-    setx PATH "$env:Path;C:\PROGRA~1\Amazon\AWSCLI\bin"
-    setx AWS_DEFAULT_REGION $SetRegion
+    start-process $SetLocation_AwsCLI\$AWSCLI_Installer_File /qn -Wait
+    setx PATH "$env:Path;C:\PROGRA~1\Amazon\AWSCLI\bin" -m
+    [Environment]::SetEnvironmentVariable("AWS_DEFAULT_REGION", $SetRegion, "Machine")
     
 }
 
