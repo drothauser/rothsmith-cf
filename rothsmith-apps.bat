2513760dfe86675286f170ed0759cb4d52c03d57
Douglas Rothauser <drothauser@yahoo.com>
2019-01-03 22:07:32 -0500

    Development

--- rothsmith-apps.bat
+++ rothsmith-apps.bat
@@ -8,22 +8,30 @@ if "%1"=="" (
    echo Missing stack name argument.
    goto syntax
 )
-
 set STACKNAME=%1
 
-aws cloudformation delete-stack --stack-name %STACKNAME%
+aws cloudformation create-stack^
+ --capabilities CAPABILITY_IAM ^
+ --disable-rollback ^
+ --stack-name %STACKNAME%^
+ --template-body file://rothsmith-apps.yaml^
+ --parameters^
+    ParameterKey=VPCStack,ParameterValue=^"ROTHSMITH-VPC^" ^
+    ParameterKey=S3Bucket,ParameterValue=^"rothsmith-cloudformation^" 
+
 set RC=%ERRORLEVEL%
 goto finish
 
 :syntax
    echo Syntax: %0 [stack name] 
    echo Example:
-   echo    %0 mystack
+   echo    %0 mystack 
    set RC=1
 
 :finish
 echo.
 echo ***********************************************************************
-echo * Delete Stack Submitted to AWS. RC = %RC%
+echo * Stack launch submitted to AWS. RC = %rc%
 echo ***********************************************************************
+endlocal
 exit /B %RC%
