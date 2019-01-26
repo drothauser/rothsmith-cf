35e5b3ee83a17656bd4454a4097a3bac35b3f00e
Douglas Rothauser <drothauser@yahoo.com>
2019-01-26 13:24:31 -0500

    Development

--- rothsmith-vpc.bat
+++ rothsmith-vpc.bat
@@ -2,19 +2,21 @@
 setlocal
 
 :: = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-:: Validate stack name argument
+:: Evaluate argument
 :: = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-if "%1"=="" (
-   echo Missing stack name argument.
+if "%1"=="--help" (
    goto syntax
 )
-set STACKNAME=%1
+set TEMPLATE_URL="https://s3.amazonaws.com/rothsmith-cloudformation/rothsmith-vpc.yaml"
+if "%1"=="--file" (
+   set TEMPLATE_URL="file://rothsmith-vpc.yaml"   
+)
 
 aws cloudformation create-stack^
  --capabilities CAPABILITY_IAM ^
  --disable-rollback ^
- --stack-name %STACKNAME%^
- --template-body file://rothsmith-vpc.yaml^
+ --stack-name ROTHSMITH-VPC^
+ --template-body %TEMPLATE_URL% ^
  --parameters^
     ParameterKey=KeyPairName,ParameterValue=^"RothsmithKeyPair^"  
 
@@ -22,9 +24,10 @@ set RC=%ERRORLEVEL%
 goto finish
 
 :syntax
-   echo Syntax: %0 [stack name] 
-   echo Example:
-   echo    %0 mystack 
+   echo "Syntax: %0 [--file | --help]"
+   echo Examples:
+   echo    %0 --file   Use local template file to launch stack i.e. file://rothsmith-vpc.yaml
+   echo    %0 --help   Command usage
    set RC=1
 
 :finish
