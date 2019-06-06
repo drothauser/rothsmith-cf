efeb371341f39675c78619910ea1fa7656dcbbb4
Doug Rothauser <drothauser@yahoo.com>
2019-06-06 12:37:50 -0400

    Jenkins job initialization script

--- jobsinit.sh
+++ jobsinit.sh
@@ -4,4 +4,6 @@
 find /var/lib/jenkins/jobs -maxdepth 2 -mindepth 2 -name lastS* -exec rm -rf {} \;
 find /var/lib/jenkins/jobs -maxdepth 2 -mindepth 2 -type d -exec rm -rf {} \;
 find /var/lib/jenkins/jobs -maxdepth 2 -mindepth 2 -name nextBuildNumber -exec sh -c 'echo 1 > {}'  \;
+rm -rf /var/lib/jenkins/workspace
+
 
