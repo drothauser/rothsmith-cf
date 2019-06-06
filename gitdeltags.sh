edf7d6eb339750105ed1241a62dc3c88232a7e22
Doug Rothauser <drothauser@yahoo.com>
2019-06-06 12:49:11 -0400

    Jenkins job initialization script

---
+++ gitdeltags.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git ls-remote -t | awk '{print $2}' | cut -d '/' -f 3 | cut -d '^' -f 1  | sort -b -t . -k 1,1nr -k 2,2nr -k 3,3r -k 4,4r -k 5,5r | uniq | xargs git push --delete origin
