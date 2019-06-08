3082bd84cde66d21bd75eceb8414d9ea9ad503a9
Douglas Rothauser <drothauser@yahoo.com>
2019-06-08 18:02:54 -0400

    devops

--- nexus-route53.bat
+++ nexus-route53.bat
@@ -1,14 +1,19 @@
 @echo off
 setlocal
 
-nexusZone=$(aws route53 list-hosted-zones-by-name --dns-name "nexus.rothsmith.net" --max-items 1 | grep '"Id"' | sed "s/^.*\/\(.*\)\"\,/\1/g")
+REM nexusZone=$(aws route53 list-hosted-zones-by-name --dns-name "nexus.rothsmith.net" --max-items 1 | grep '"Id"' | sed "s/^.*\/\(.*\)\"\,/\1/g")
+set zoneCmd='aws route53 list-hosted-zones-by-name --dns-name ^"nexus.rothsmith.net^" --max-items 1 | findstr ^"\^"Id\^":^"'
+FOR /F "tokens=* USEBACKQ" %%F IN (`%zoneCmd%`) DO (
+   SET var=%%F
+)
+ECHO %var%
 
-nexusElb=$(aws elbv2 describe-load-balancers | grep -i nexus | grep DNSName | awk '{print $2}' | sed 's/[\"\,]//g')
+REM nexusElb=$(aws elbv2 describe-load-balancers | grep -i nexus | grep DNSName | awk '{print $2}' | sed 's/[\"\,]//g')
 
-echo "****** ${nexusElb} *****"
+REM echo "****** ${nexusElb} *****"
 
 set /P nexusJson=<./nexus-route53.json
 
 REM nexusJson=$(cat ./nexus-route53.json | sed "s/XXXXXXX/${nexusElb}/")
 
-aws route53 change-resource-record-sets --hosted-zone-id $nexusZone --change-batch "%nexusJson%"
+REM aws route53 change-resource-record-sets --hosted-zone-id $nexusZone --change-batch "%nexusJson%"
