9d30a3c138ecdfe79e7c6eb6695ca6205000a7fd
Doug Rothauser <drothauser@yahoo.com>
2019-06-20 17:57:05 -0400

    git stuff

--- git-delete-tags.sh
+++ git-delete-tags.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-git ls-remote -t | awk '{print $2}' | cut -d '/' -f 3 | cut -d '^' -f 1  | sort -b -t . -k 1,1nr -k 2,2nr -k 3,3r -k 4,4r -k 5,5r | uniq | xargs git push --delete origin
+if git ls-remote -t | awk '{print $2}' | cut -d '/' -f 3 | cut -d '^' -f 1  | sort -b -t . -k 1,1nr -k 2,2nr -k 3,3r -k 4,4r -k 5,5r | uniq | xargs git push --delete origin 
+then 
+  echo "deleted remote tags"
+fi
 
-git ls-remote -t | awk '{print $2}' | cut -d '/' -f 3 | cut -d '^' -f 1  | sort -b -t . -k 1,1nr -k 2,2nr -k 3,3r -k 4,4r -k 5,5r | uniq | xargs git tag --delete
-
+if git ls-remote -t | awk '{print $2}' | cut -d '/' -f 3 | cut -d '^' -f 1  | sort -b -t . -k 1,1nr -k 2,2nr -k 3,3r -k 4,4r -k 5,5r | uniq | xargs git tag --delete 
+then
+  echo "deleted local tags"
+fi
