c2c7b82818c570849aa40639852996b2241eaec0
Doug Rothauser <drothauser@yahoo.com>
2021-02-26 10:07:38 -0500

    improve aws cli commands

--- rothsmith-route53.sh
+++ rothsmith-route53.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-rothsmithZone=$(aws route53 list-hosted-zones-by-name --dns-name "rothsmith.net" --max-items 1 | grep '"Id"' | sed "s/^.*\/\(.*\)\"\,/\1/g")
+#rothsmithZone=$(aws route53 list-hosted-zones-by-name --dns-name "rothsmith.net" --max-items 1 | grep '"Id"' | sed "s/^.*\/\(.*\)\"\,/\1/g")
 
-rothsmithElb=$(aws elbv2 describe-load-balancers | grep -i 'web-public' | grep DNSName | awk '{print $2}' | sed 's/[\"\,]//g')
+rothsmithZone=$(aws route53 list-hosted-zones-by-name --dns-name "rothsmith.net" --max-items 1 --query 'HostedZones[*].Id' --output text | sed 's/\/hostedzone\///g')
 
-echo "****** ${rothsmithElb} *****"
+lbId="web-public"
+if [[ ! -z ${1} ]]; then lbId="${1}"; fi
+
+#rothsmithElb=$(aws elbv2 describe-load-balancers | grep -i "${lbId}" | grep DNSName | awk '{print $2}' | sed 's/[\"\,]//g')
+rothsmithElb=$(aws elbv2 describe-load-balancers --query "LoadBalancers[?starts_with(DNSName,'test')].DNSName" --output text)
 
 rothsmithJson=$(cat ./rothsmith-route53.json | sed "s/XXXXXXX/${rothsmithElb}/")
+
+echo -e "*********************************************
+*  rothsmithZone=${rothsmithZone}
+*  rothsmithElb=${rothsmithElb}
+*  rothsmithJson=
+${rothsmithJson}
+*
+*********************************************"
 
 aws route53 change-resource-record-sets --hosted-zone-id $rothsmithZone --change-batch "${rothsmithJson}"
