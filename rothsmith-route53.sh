e9aabeb71cadf333d2b2c54052a6162137a6d500
Doug Rothauser <drothauser@yahoo.com>
2021-03-05 17:51:23 -0500

    Route53 enhancements

--- rothsmith-route53.sh
+++ rothsmith-route53.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
 
-loadBalancerPrefix="web-public"
-if [[ ! -z ${1} ]]; then loadBalancerPrefix="${1,,}"; fi
+subdomain="web-public"
+if [[ ! -z ${1} ]]; then subdomain="${1,,}"; fi
 
-#rothsmithZone=$(aws route53 list-hosted-zones-by-name --dns-name "rothsmith.net" --max-items 1 | grep '"Id"' | sed "s/^.*\/\(.*\)\"\,/\1/g")
+rothsmithZone=$(aws route53 list-hosted-zones-by-name --dns-name "${subdomain}.rothsmith.net" --max-items 1 --query 'HostedZones[*].Id' --output text | sed 's/\/hostedzone\///g')
 
-rothsmithZone=$(aws route53 list-hosted-zones-by-name --dns-name "${loadBalancerPrefix}.rothsmith.net" --max-items 1 --query 'HostedZones[*].Id' --output text | sed 's/\/hostedzone\///g')
+rothsmithElb=$(aws elbv2 describe-load-balancers | grep -i "${subdomain}" | grep DNSName | awk '{print $2}' | sed 's/[\"\,]//g')
 
-rothsmithElb=$(aws elbv2 describe-load-balancers | grep -i "${loadBalancerPrefix}" | grep DNSName | awk '{print $2}' | sed 's/[\"\,]//g')
-#rothsmithElb=$(aws elbv2 describe-load-balancers --query "LoadBalancers[?starts_with(DNSName,'test')].DNSName" --output text)
 
-rothsmithJson=$(cat ./rothsmith-route53.json | sed "s/XXXXXXX/${rothsmithElb}/")
+read -r -d '' CHANGE_BATCH <<-_EOF_
+{
+    "Comment": "Update Rothsmith hosted zone alias resource record sets in Route 53",
+    "Changes": [
+        {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "${subdomain}.rothsmith.net",
+                "Type": "A",
+                "AliasTarget": {
+                    "HostedZoneId": "Z35SXDOTRQ7X7K",
+                    "DNSName": "dualstack.${rothsmithElb}.",
+                    "EvaluateTargetHealth": false
+                }
+            }
+        }
+    ]
+}
+_EOF_
 
 echo -e "*********************************************
+*  subdomain=${subdomain}
 *  rothsmithZone=${rothsmithZone}
 *  rothsmithElb=${rothsmithElb}
 *  rothsmithJson=
-${rothsmithJson}
-*
-*********************************************"
+${CHANGE_BATCH}"
 
-aws route53 change-resource-record-sets --hosted-zone-id $rothsmithZone --change-batch "${rothsmithJson}"
+COMMAND="aws route53 change-resource-record-sets --hosted-zone-id ${rothsmithZone} --change-batch '${CHANGE_BATCH}'"
+echo ${COMMAND}
+eval "${COMMAND}"
+
+RC="$?"
+echo -e "\nCommand Complete. Return Code = ${RC}"
+exit ${RC}
